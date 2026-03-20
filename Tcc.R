d757a693e8011de60de789262c9255bd45a1d80c
PedroPaixao-1 <pedropaxprofissional@gmail.com>
2026-03-20 15:25:42 -0300

    Add files via upload

---
+++ Tcc.R
@@ -0,0 +1,48 @@
+source("Code/Tidyverse_function.R")
+
+Arquivo_pdb <- read.pdb("/home/pedro.paixao/Code/egfr_padronizado2.pdb")
+Arquivo_pdb_ca <- atom.select(Arquivo_pdb, "calpha")
+Arquivo_pdb_ca <- trim.pdb(Arquivo_pdb, inds = Arquivo_pdb_ca)
+
+Contatos_EGFR <- Calcular_Contato(Arquivo_pdb_ca, 8)
+
+Amostras <- 200
+
+Sequencia <- pdbseq(Arquivo_pdb)
+Sequencia <- Sequencia[1:613]
+Sequencia <- paste(Sequencia, collapse = "")
+
+Temperatura <- 1.5
+
+Porcentagem <- c(20,30,40,50,60,70,80,90,100)
+Variantes <- list()
+Multi_fasta <- list()
+
+j <- 1
+
+while (j <= 200) {
+  
+  
+  Posicao <- sample(Contatos_EGFR, size = ceiling(Porcentagem[[j]]/100*length(Contatos_EGFR)))
+  unlist(Posicao)
+  
+  Comando <- paste("/home/conda/condabin/conda run -n esm2","python /home/pedro.paixao/Code/generate_sequence_esm2.py", "--sequence", Sequencia, "--position", Posicao,"--temperature", Temperatura)
+  #Isso aqui vai dar merda por causa do diretório na rtx
+  system(Comando, wait = TRUE)
+  
+  Saida <- readLines("completed_sequence.txt")
+  #Variantes[[i]] <- Saida
+  if (Saida %in% Variantes){
+    next
+  }
+  Header <- paste(">EGFR|CHAIN A|Variante",j)
+  Multi_fasta[[j]] <- paste(Header,Saida, sep = "\n")
+  Arquivo_fasta <- paste("Variante_",Porcentagem[[j]],".fasta")
+  writeLines(unlist(Multi_fasta), "Variantes.fasta")
+  j = j+1
+}
+# pdb_filtrado <- Processamento_pdb(Arquivo_pdb_padronizado,'CA','Filtrar')
+# Contatos <- Processamento_pdb(Arquivo_pdb_padronizado,'CA','Contato')
+
+
+
